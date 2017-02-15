d00aebd673f37f62e6d4b5f9b21875371271aa4d
LightReason <15839956590@163.com>
2017-02-15 14:43:37 +0800

    init respose

---
+++ Dilidili/Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dilidili
+//
+//  Created by 刘强 on 2017/2/15.
+//  Copyright © 2017年 LightReason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dilidili/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dilidili
+//
+//  Created by 刘强 on 2017/2/15.
+//  Copyright © 2017年 LightReason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
